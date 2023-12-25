c4eeca5bd4b9f486cac5f6f02195b589a3e66292
mp-web3 <mp.web3.t@gmail.com>
2023-12-25 12:35:39 +0100

    basic serverless api ready

--- api/videos.js
+++ api/videos.js
@@ -4,10 +4,10 @@ require('dotenv').config({path: "../.env"});
 module.exports = async (req, res) => {
     try {
         const db = await connectToDatabase();
-        const collection = await db.collection(process.env.NEWS);
+        const collection = await db.collection(process.env.VIDEOS);
 
-        const newsArray = await collection.find().limit(6).toArray();
-        res.status(200).json(newsArray);
+        const videosArray = await collection.find().limit(6).toArray();
+        res.status(200).json(videosArray);
     } catch (error) {
         res.status(500).send("Error: " + error.message);
     }
