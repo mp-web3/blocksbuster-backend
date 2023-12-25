7c8b0a5ce6903a53686e5835e7cdaaaebfbeb87a
mp-web3 <mp.web3.t@gmail.com>
2023-12-25 12:12:34 +0100

    first commit setup

---
+++ api/news.js
@@ -0,0 +1,15 @@
+module.exports = (req, res) => {
+  res.json([
+    {
+      id: "The 4 Best Blockchain Stocks (Beyond Cryptocurrency)",
+      title: "The 4 Best Blockchain Stocks (Beyond Cryptocurrency)",
+      description:
+        "InvestorPlace - Stock Market News, Stock Advice & Trading Tips The swi…",
+      publishedAt: "2023-11-21T05:43:06Z",
+      author: "Business Insider",
+      image:
+        "https://investorplace.com/wp-content/uploads/2021/12/square-block-1600…",
+      url: "https://markets.businessinsider.com/news/stocks/the-4-best-blockchain-…",
+    },
+  ]);
+};
